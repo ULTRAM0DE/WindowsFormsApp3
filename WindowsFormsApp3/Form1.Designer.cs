b5e7220670324c070d132039217e7ec987da2cae
0NE <0netw08778@gmail.com>
2020-04-14 19:34:45 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Background = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.gamepanel = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.Background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamepanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Background
+            // 
+            this.Background.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Background.Controls.Add(this.pictureBox);
+            this.Background.Controls.Add(this.gamepanel);
+            this.Background.Location = new System.Drawing.Point(1, -2);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(929, 497);
+            this.Background.TabIndex = 0;
+            this.Background.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Background_PreviewKeyDown);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox.Location = new System.Drawing.Point(188, 210);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(40, 35);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // gamepanel
+            // 
+            this.gamepanel.BackColor = System.Drawing.Color.Red;
+            this.gamepanel.Location = new System.Drawing.Point(267, 425);
+            this.gamepanel.Name = "gamepanel";
+            this.gamepanel.Size = new System.Drawing.Size(164, 15);
+            this.gamepanel.TabIndex = 1;
+            this.gamepanel.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Background.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gamepanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Background;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox gamepanel;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private int speed_vertical = 4;
+        private int speed_hor = 4;
+        private int score = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Enabled = true;
+
+            Cursor.Hide();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            _ = gamepanel.Top - Background.Bottom - (Background.Bottom / 10);
+
+        }
+
+        private void Background_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                this.Close();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            gamepanel.Left = Cursor.Position.X - (gamepanel.Width / 2);
+
+            pictureBox.Left += speed_hor;
+            pictureBox.Top += speed_vertical;
+
+            if (pictureBox.Left <= Background.Left)
+                speed_hor *= -1;
+
+            if (pictureBox.Right >= Background.Right)
+                speed_hor *= -1;
+
+            if (pictureBox.Top <= Background.Top)
+                speed_vertical *= -1;
+
+            if (pictureBox.Bottom >= Background.Bottom)
+                timer.Enabled = false;
+
+            if (pictureBox.Bottom >= gamepanel.Top && pictureBox.Bottom <= gamepanel.Bottom
+                && pictureBox.Left >= gamepanel.Left && pictureBox.Right <= gamepanel.Right)
+            {
+                speed_hor += 3;
+                speed_vertical += 3;
+                speed_vertical *= -1;
+                score += 1;
+
+            }   
+        }
+    }
+}
